af6029a8db8c4df72d1822c47ad2809b95d1e32f
FrenchYeti <georges.michel@protonmail.com>
2020-08-07 07:37:18 +0200

    add ios analyzer

--- src/ios/IosAppAnalyzer.ts
+++ src/ios/IosAppAnalyzer.ts
@@ -1,30 +1,16 @@
-import {Device} from "../Device";
-import {ApplicationBinary} from "../Application";
+import IosApplication from "./IosApplication";
+import DexcaliburProject from "../DexcaliburProject";
 
-export default class IosApplication extends ApplicationBinary
+
+export default class IosAppAnalyzer
 {
-    constructor() {
-        super();
+    ctx:DexcaliburProject;
+
+    constructor(pContext:DexcaliburProject) {
+        this.ctx = pContext;
     }
 
-    start(pDevice:Device):number{
-        throw new Error('[IOS APPLICATION] "start" operation is not implemented.');
-        return -1;
-    }
-
-    kill(pDevice:Device, pPID:number=-1):boolean{
-        throw new Error('[IOS APPLICATION] "kill" operation is not implemented.');
-        return false;
-    }
-
-    isRunning(pDevice:Device):boolean{
-        throw new Error('[IOS APPLICATION] "isRunning" operation is not implemented.');
-        return false;
-    }
-
-    getPID(pDevice:Device):number{
-        throw new Error('[IOS APPLICATION] "getPID" operation is not implemented.');
-
-        return -1;
+    async scan(path:string):Promise<IosApplication>{
+        return new IosApplication(this.ctx);
     }
 }
