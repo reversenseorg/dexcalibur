86beb3fef7754279edee702fce0be1ca7663e2b5
FrenchYeti <georges.michel@protonmail.com>
2020-11-02 09:00:55 +0100

    Add user profile

---
+++ src/User.ts
@@ -0,0 +1,65 @@
+/**
+ * To represent a user
+ */
+import DexcaliburProject from "./DexcaliburProject";
+import {TerminalSession} from "./TerminalSession";
+
+export class Team {
+    name:string ='';
+    color:string = 'red';
+    members: User[] = [];
+    owners: User[] = [];
+    projects: DexcaliburProject[] = [];
+}
+
+interface TerminalSessionMap {
+    [localid:string] : TerminalSession;
+}
+
+
+export class User {
+
+    private uid:string ='x-x-x-x';
+
+    firstname: string = '';
+    lastname:string = '';
+    team: Team = null;
+
+    termSessions:TerminalSessionMap = {};
+
+    readonly dateCreate:string = '';
+    readonly dateLastModified:string = '';
+
+    constructor(pConfig:any) {
+        for(let i in pConfig)
+            if(this.hasOwnProperty(i))
+                this[i] = pConfig[i];
+    }
+
+    /**
+     * To compare two user
+     *
+     * @param pUser
+     */
+    is(pUser:User):boolean {
+        return (this.uid == pUser.uid);
+    }
+
+    getTerminalSession( pSessID:string):void {
+        let term:TerminalSession = null;
+
+    }
+
+    addSession( pSession:TerminalSession, pLocalID:string):void {
+        this.termSessions[pLocalID] = pSession;
+    }
+
+    getLocalIdOf( pSession:TerminalSession):string {
+        for(let lid in this.termSessions){
+            if(this.termSessions[lid].getSessionID() === pSession.getSessionID()){
+                return lid;
+            }
+        }
+        return null;
+    }
+}
