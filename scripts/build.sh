dad5017bba69f7eea955ff4c66fb1241854f2c45
FrenchYeti <georges.michel@protonmail.com>
2021-03-12 01:00:25 +0100

    fix hook requires into build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,11 @@ echo "[+] Transpiling sources to JS"
 mkdir ./dist
 cp ./package.json ./dist/package.json
 cp ./package.json ./dist/dexcalibur-ts/package.json
-cp -r ./test ./dist/dexcalibur-ts/test
 tsc
 
 echo "[+] Copying files"
 grunt build
+
+
+echo "[+] Move files : hook requirements, etc .."
+mv ./dist/src/requires ./dist/dexcalibur-ts/src/requires
