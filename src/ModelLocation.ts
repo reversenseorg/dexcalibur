0b843dee7e58669804b08a2f41d35e6894c3790c
FrenchYeti <georges.michel@protonmail.com>
2021-04-05 18:33:28 +0200

    [CORE] Add a class modeling location of something into a file or memory

---
+++ src/ModelLocation.ts
@@ -0,0 +1,61 @@
+import ModelFile from "./ModelFile";
+import {NodeType} from "./NodeType";
+
+export enum LocationType {
+    FILE,
+    MEM
+}
+
+/**
+ * @class
+ */
+export class ModelLocation {
+
+    _t:LocationType = LocationType.FILE;
+    _p:any = {};
+
+
+    constructor(pConfig:any=null) {
+        if(pConfig!=null)
+            for(let i in pConfig)
+                this[i] = pConfig[i];
+    }
+
+    set file(pFile:ModelFile) {
+        this._p.file = pFile;
+    }
+
+    get file():ModelFile {
+        return this._p.file;
+    }
+
+    static fromFile(pFile:ModelFile):ModelLocation {
+        let o:ModelLocation = new ModelLocation();
+        o.file = pFile;
+        return o;
+    }
+
+    /**
+     *
+     */
+    isFileBased():boolean {
+        return (this._t == LocationType.FILE);
+    }
+
+    /**
+     *
+     */
+    toJsonObject(){
+        if(this._t === LocationType.FILE){
+            return {
+                _t: NodeType.FILE,
+                _f: this.file.toJsonObject()
+            };
+        }else{
+            return {
+                _t: null,
+                _f: null
+            };
+        }
+    }
+}
