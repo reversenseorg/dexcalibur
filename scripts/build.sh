ea20942539817b7e4387fb15b7da9bdaf077ddd7
FrenchYeti <georges.michel@protonmail.com>
2021-04-09 17:32:15 +0200

    [SCRIPT] Move "move command" from bash script to gruntfile

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,17 +4,19 @@ rm -r ./dist
 
 echo "[+] Transpiling sources to JS"
 mkdir ./dist
+tsc
+
+echo "[+] Copying package.json"
 cp ./package.json ./dist/package.json
 cp ./package.json ./dist/dexcalibur-ts/package.json
-tsc
 
 echo "[+] Copying files"
 grunt build
 
 
-echo "[+] Move files : hook requirements, etc .."
-mv ./dist/src/requires ./dist/dexcalibur-ts/src/requires
+# echo "[+] Move files : hook requirements, etc .."
+# mv ./dist/src/requires ./dist/dexcalibur-ts/src/requires
 
-echo "[+] Move files : binaries."
-mv ./dist/bin ./dist/dexcalibur-ts/bin
+# echo "[+] Move files : binaries."
+# mv ./dist/bin ./dist/dexcalibur-ts/bin
 
