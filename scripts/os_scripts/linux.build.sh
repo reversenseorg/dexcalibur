b77c8a35ab3781dc128759c80287df3ad5ab4874
FrenchYeti <georges.michel@protonmail.com>
2021-05-25 12:01:18 +0200

    [CICD] replace grunt my os specific script

---
+++ scripts/os_scripts/linux.build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+echo "[+] Removing ./dist folder"
+rm -r ./dist
+
+echo "[+] Transpiling sources to JS"
+mkdir ./dist
+tsc
+
+#echo "[+] Copying package.json"
+#cp ./package.json ./dist/dexcalibur-ts/package.json
+
+echo "[+] Copying files"
+cp -r ./scripts/ ./dist/scripts
+cp -r ./node_modules/ ./dist/node_modules
+cp -r ./package.json ./dist/package.json
+cp -r ./info.json ./dist/info.json
+cp -r ./package-lock.json ./dist/package-lock.json
+cp -r ./dexcalibur ./dist/dexcalibur
+cp -r ./README.md ./dist/scripts
+cp -r ./src/requires ./dist/src/requires
+
+for i in ./inspectors/*/web
+do
+  [[ -d "$i" ]] || break
+  cp -r $i ./dist/$i
+done
