f39af28e54bf24de2c32c8ab6d3fcec74b2b6379
FrenchYeti <georges.michel@protonmail.com>
2021-08-30 09:17:53 +0200

    Add additional arg support

--- scripts/os_scripts/darwin.postbuild-electron.sh
+++ scripts/os_scripts/darwin.postbuild-electron.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
-echo "[+] Setup Electron Frida-binding"
-cp ./scripts/frida/electron_frida_binding.node ./dist/node_modules/frida/build/Release/frida_binding.node
+echo "[+] Setup Electron Frida-binding into 'dist' "
+
+if [ $# -lt 1 ]; then
+  echo "[+] Copying default file ' ./scripts/frida/electron_frida_binding.node ' "
+  cp ./scripts/frida/electron_frida_binding.node ./dist/node_modules/frida/build/Release/frida_binding.node
+else
+  echo "[+] Copying ' ./scripts/frida/electron_frida_binding."$1".node ' in 'dist' folder"
+  if [ -f "./scripts/frida/electron_frida_binding."$1".node" ]; then
+    cp "./scripts/frida/electron_frida_binding."$1".node" ./dist/node_modules/frida/build/Release/frida_binding.node
+  else
+    echo "\x1b[31m[!] Error : File ' ./scripts/frida/electron_frida_binding."$1".node ' not found \x1b[0m"
+  fi
+fi
+
--- scripts/os_scripts/darwin.prebuild-electron.sh
+++ scripts/os_scripts/darwin.prebuild-electron.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 
 echo "[+] Setup Electron Frida-binding"
-cp ./scripts/frida/electron_frida_binding.node ./node_modules/frida/build/Release/frida_binding.node
+
+if [ $# -lt 1 ]; then
+  echo "[+] Copying default file ' ./scripts/frida/electron_frida_binding.node ' "
+  cp ./scripts/frida/electron_frida_binding.node ./node_modules/frida/build/Release/frida_binding.node
+else
+  echo "[+] Copying ' ./scripts/frida/electron_frida_binding."$1".node ' "
+  if [ -f "./scripts/frida/electron_frida_binding."$1".node" ]; then
+    cp "./scripts/frida/electron_frida_binding."$1".node" ./node_modules/frida/build/Release/frida_binding.node
+  else
+    echo "\x1b[31m[!] Error : File ' ./scripts/frida/electron_frida_binding."$1".node ' not found \x1b[0m"
+  fi
+fi
+
