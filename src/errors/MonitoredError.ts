78a6d45ca00b0d3c5d99fb37e0a1b512fd865bd0
FrenchYeti <georges.michel@protonmail.com>
2021-08-30 09:18:58 +0200

    Add base class for exwception

---
+++ src/errors/MonitoredError.ts
@@ -0,0 +1,57 @@
+export enum ErrorCode {
+    GENERIC= 1000,
+    DEVICE_MANAGER= 2000,
+    ANALYZER= 3000,
+    AUTH=30000,
+    PARSER = 40000,
+    SMALI_PARSER = 41000,
+    SECURITY = 90000,
+    REMOTE = 20000,
+    REMOTE_DEXCALIBUR= 21000
+}
+
+export class MonitoredError extends Error {
+
+    /**
+     * Component name
+     *
+     * @field
+     * @type string
+     */
+    cmp:string;
+    code:number;
+    extra:any;
+
+    constructor( pCmp:string, pMsg:string, pCode:number = null, pExtra:any = null) {
+        super(pMsg);
+        this.cmp = pCmp;
+        this.code = pCode;
+        this.extra = pExtra;
+    }
+
+    getCode():number {
+        return this.code;
+    }
+
+
+    getExtra():string {
+        return this.extra;
+    }
+
+    toString():string {
+        return `[${this.cmp}] [#${this.code!=null ? this.code : "<null>"} ${this.message}`;
+    }
+
+    /**
+     *
+     * @param pIncludeExtra
+     */
+    toObject(pIncludeExtra:boolean=false):any {
+        return {
+            cmp: this.cmp,
+            code: this.code,
+            msg: this.message,
+            extra: pIncludeExtra ? this.extra : null
+        }
+    }
+}
