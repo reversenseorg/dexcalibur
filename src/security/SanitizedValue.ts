ec8925692ac7b6bc5865cac0715fb5ee848d1db7
Georges-B. Michel <georges@reversense.com>
2021-09-13 20:21:20 +0200

    add sanitized values (rasp)

---
+++ src/security/SanitizedValue.ts
@@ -0,0 +1,50 @@
+import {RuntimeSecurityException} from "../errors/RuntimeSecurityException";
+
+
+export abstract class IncomingValue {
+    protected name:string;
+    protected value:any;
+
+    constructor( pName:string, pValue:any) {
+        this.name = pName;
+        this.value = pValue;
+    }
+
+    abstract getName(): string;
+    abstract getValue(): any;
+}
+
+export class SanitizedValue extends IncomingValue {
+
+    getName(): string {
+        return this.name;
+    }
+
+    getValue(): any {
+        return this.value;
+    }
+}
+
+export class PassthroughValue extends IncomingValue {
+
+    getName(): string {
+        return this.name;
+    }
+
+    getValue(): any {
+        return this.value;
+    }
+}
+
+
+
+export class UnsafeValue extends IncomingValue {
+
+    getName(): string {
+        return this.name;
+    }
+
+    getValue(): any {
+        throw RuntimeSecurityException.USE_OF_UNSAFE_VALUE(new PassthroughValue( this.name, this.value));
+    }
+}
