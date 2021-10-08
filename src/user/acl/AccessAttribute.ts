76eca023ff9802f6ccc078dcedc619d761b3c052
Georges-B. Michel <georges@reversense.com>
2021-10-08 11:12:48 +0200

    [ACL] Add access attributes

---
+++ src/user/acl/AccessAttribute.ts
@@ -0,0 +1,61 @@
+import {Access, AccessType} from "./Access";
+
+
+export interface AccessAttributeMap {
+    [uid:string] :AccessAttribute
+}
+
+/**
+ * Represents an access point
+ */
+export class AccessAttribute {
+
+    private _n:string;
+    private _v:any;
+
+    constructor( pName:string, pValue:any = null) {
+        this._n = pName;
+        this._v= pValue;
+    }
+
+
+    get name():string {
+        return this._n;
+    }
+
+    get value():string {
+        return this._v;
+    }
+
+
+    set value(v:string) {
+        this._v = v;
+    }
+
+    /**
+     * To create an instance from a poor object
+     * @param {any} pData
+     */
+    static from(pData:any):AccessAttribute {
+        return new AccessAttribute(pData.name, (pData.hasOwnProperty('value')? pData.value : null));
+    }
+
+    /**
+     * To clone without value
+     *
+     */
+    clone():AccessAttribute {
+        return new AccessAttribute(this._n);
+    }
+
+    /**
+     * To serialize into poor object ready to be serialized
+     *
+     */
+    toJsonObject():any {
+        return {
+            _n: this._n,
+            _v: (this._v != null && this._v.hasOwnProperty('toJsonObject'))? this._v.toJsonObject() : this._v
+        };
+    }
+}
