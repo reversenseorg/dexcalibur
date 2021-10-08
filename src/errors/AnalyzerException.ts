79ba125121d06f98ef57d7b771efb4e794692ca7
Georges-B. Michel <georges@reversense.com>
2021-10-08 11:09:57 +0200

    [ANALYZER] Add analyzer exception type

---
+++ src/errors/AnalyzerException.ts
@@ -0,0 +1,20 @@
+import {ErrorCode, MonitoredError} from "./MonitoredError";
+
+export enum AnalyzerErrCode {
+    GENERIC=1000,
+    JAVA=2000,
+    OBJC=3000,
+    NATIV=4000
+}
+
+export class AnalyzerException extends MonitoredError {
+
+
+    static UNKNOW_ANAL = (pABI:string)=>{
+        return new AnalyzerException("The ABI '"+pABI+"' is not supported. Please, fill an issue.",
+            ErrorCode.ANALYZER_NATIV + 201) };
+
+    constructor( pMsg:string, pCode:number = null, pExtra:any = null) {
+        super('GENERIC ANALYZER', pMsg, pCode, pExtra);
+    }
+}
