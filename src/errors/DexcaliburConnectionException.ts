cead291a689fba113fad87d6074caffb9fdb31c6
FrenchYeti <georges.michel@protonmail.com>
2021-10-11 10:25:35 +0200

    [ERROR HANDLER] add typology

--- src/errors/DexcaliburConnectionException.ts
+++ src/errors/DexcaliburConnectionException.ts
@@ -10,6 +10,7 @@ export class DexcaliburConnectionException extends MonitoredError {
     static IP_NOT_DEFINED = ()=>{ return new DexcaliburConnectionException("IP address is not defined", ErrorCode.REMOTE_DEXCALIBUR + 401) };
     static PORT_NOT_DEFINED = ()=>{ return new DexcaliburConnectionException("Port number is not defined", ErrorCode.REMOTE_DEXCALIBUR + 402) };
     static HOSTNAME_NOT_DEFINED = ()=>{ return new DexcaliburConnectionException("Host name is not defined", ErrorCode.REMOTE_DEXCALIBUR + 403) };
+    static REMOTE_OPERATION_NOT_SUPPORTED = ()=>{ return new DexcaliburConnectionException("This operation is not yet supported for remote instance.", ErrorCode.REMOTE_DEXCALIBUR + 404) };
 
     constructor( pMsg:string, pCode:number = null, pExtra:any = null) {
         super('REMOTE CONNECTION', pMsg);
---
+++ src/errors/DexcaliburProjectException.ts
@@ -0,0 +1,26 @@
+import {ErrorCode, MonitoredError} from "./MonitoredError";
+
+export class DexcaliburProjectException extends MonitoredError {
+
+    code:number;
+    extra:any;
+
+    static ALL = {};
+
+    static TARGET_DEVICE_NOT_FOUND = ()=>{ return new DexcaliburProjectException("Target device is not found", ErrorCode.PROJECT + 101) };
+    static TARGET_DEVICE_NOT_ENROLLED = ()=>{ return new DexcaliburProjectException("Target device is not enrolled or ready", ErrorCode.PROJECT + 102) };
+    static INVALID_NAME = ()=>{ return new DexcaliburProjectException("Project name is invalid", ErrorCode.PROJECT + 103) };
+    static APP_FILE_OT_FOUND = ()=>{ return new DexcaliburProjectException("App file is not found", ErrorCode.PROJECT + 104) };
+    static STEP2_FAILURE = (pMsg:string='')=>{ return new DexcaliburProjectException("[STEP 2] Creating new project failed : "+pMsg, ErrorCode.PROJECT + 105) };
+    static NEW_PROJECT_FAIL = ()=>{ return new DexcaliburProjectException("Project cannot be created. See logs for more details. ", ErrorCode.PROJECT + 106) };
+    static OPEN_PROJECT_FAILURE = ()=>{ return new DexcaliburProjectException("The project cannot be opened", ErrorCode.PROJECT + 107) };
+    static DELETE_PROJ_FAILURE_NOTFOUND = ()=>{ return new DexcaliburProjectException("Delete failure : project is not found.", ErrorCode.PROJECT + 108) };
+
+
+
+    constructor( pMsg:string, pCode:number = null, pExtra:any = null) {
+        super('PROJECT', pMsg);
+        this.code = pCode;
+        this.extra = pExtra;
+    }
+}
