9782abe6bef200408315cd76aec0e6010779eead
FrenchYeti <georges.michel@protonmail.com>
2021-10-11 10:22:44 +0200

    [ACL] replace user session by user account object into checks

--- src/user/session/UserSession.ts
+++ src/user/session/UserSession.ts
@@ -125,7 +125,7 @@ export class UserSession {
             AccessZone.PROJECT,
             ProjectAccessControl.access.PROJ_OPEN_OWN,
             pContext.getProject(pProjectUID),
-            this
+            this.getUserAccount()
         );
 
 
