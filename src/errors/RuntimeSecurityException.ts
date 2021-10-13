f39e2c08b179d7d9bd2293ba93d4df5cad5cdff9
FrenchYeti <georges.michel@protonmail.com>
2021-10-13 17:36:39 +0200

    [ERROR] add exception for path traversal

--- src/errors/RuntimeSecurityException.ts
+++ src/errors/RuntimeSecurityException.ts
@@ -3,9 +3,8 @@ import {PassthroughValue, SanitizedValue, UnsafeValue} from "../security/Sanitiz
 
 export class RuntimeSecurityException extends MonitoredError {
 
-    static USE_OF_UNSAFE_VALUE = (pUnsafeValue:PassthroughValue)=>{
-        return new RuntimeSecurityException("Value is invalid",
-            ErrorCode.SECURITY_RUNTIME + 101, pUnsafeValue) };
+    static USE_OF_UNSAFE_VALUE = (pUnsafeValue:PassthroughValue)=>{ return new RuntimeSecurityException("Value is invalid",ErrorCode.SECURITY_RUNTIME + 101, pUnsafeValue) };
+    static PATH_TRAVERSAL_IS_FORBIDDEN = ()=>{ return new RuntimeSecurityException("Path traversal is forbidden",ErrorCode.SECURITY_RUNTIME + 102) };
 
 
     constructor( pMsg:string, pCode:number = null, pExtra:any = null) {
