e57c3bc700ffab946a4c09641eb6b7c94ef8c9d0
FrenchYeti <georges.michel@protonmail.com>
2021-10-13 17:41:28 +0200

    [ERROR] Add session exception

--- src/user/session/SessionException.ts
+++ src/user/session/SessionException.ts
@@ -22,6 +22,9 @@ export class SessionException extends MonitoredError {
 
     static INVALID_SESSION = ()=>{ return new SessionException("Session is invalid", ErrorCode.AUTH + 101) };
     static EXPIRED_SESSION = ()=>{ return new SessionException("Session is expired", ErrorCode.AUTH + 102) };
+    static NO_ACTIVE_PROJECT = ()=>{ return new SessionException("This session has not active project", ErrorCode.AUTH + 103) };
+    static MULTIPLE_ACTIVE_PROJECT = ()=>{ return new SessionException("This session has multiple active project, please specify.", ErrorCode.AUTH + 104) };
+    static INVALID_ACTIVE_PROJECT_UID = (vUID:string = 'unknow')=>{ return new SessionException("This session has not active project with the given UID : "+vUID, ErrorCode.AUTH + 105) };
 
     constructor(pMsg, pCode:SessionCode = SessionCode.NONE) {
         super('SESSION',pMsg,pCode);
