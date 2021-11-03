a8a6b62b51f0445eddab14dc905a35d5f9b260a2
Georges-B. Michel <georges@reversense.com>
2021-11-03 16:58:30 +0100

    [CORE] add exception when ABI cannot be detected

--- src/errors/AbiException.ts
+++ src/errors/AbiException.ts
@@ -8,6 +8,12 @@ export class AbiException extends MonitoredError {
         return new AbiException("The ABI '"+pABI+"' is not supported. Please, fill an issue.",
             ErrorCode.ANALYZER + AnalyzerErrCode.NATIV + 1) };
 
+
+    static UNDETECTABLE_ABI = (pMsg:string)=>{
+        return new AbiException("The ABI cannot be detected : "+pMsg,
+            ErrorCode.ANALYZER + AnalyzerErrCode.NATIV + 2) };
+
+
     constructor( pMsg:string, pCode:number = null, pExtra:any = null) {
         super('ABI', pMsg, pCode, pExtra);
     }
