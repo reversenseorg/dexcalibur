ed0e0f4618baa36560355b02857a13910df3b146
Georges-B. Michel <georges@reversense.com>
2022-06-16 00:33:06 +0200

    Add documentation

--- src/Inspector.ts
+++ src/Inspector.ts
@@ -1,4 +1,9 @@
-
+/**
+ * All rights reserved at Reversense SAS 2018 - 2022
+ *
+ * @author Georges-Bastien Michel <georges@reversense.com>
+ * @copyright Reversense SAS <contact@reversense.com>
+ */
 //const UT = require("./Utils.js");
 import * as _fs_ from 'fs';
 import * as _path_ from 'path';
@@ -16,7 +21,7 @@ import {IDatabase, IDatabaseAdapter} from "./persist/orm/DbAbstraction";
 import {DelegateWebApi} from "./webapi/DelegateWebApi";
 import WebServer from "./WebServer";
 
-let Logger:Log.Logger = Log.newLogger() as Log.Logger;
+const Logger:Log.Logger = Log.newLogger() as Log.Logger;
 
 const TASK_CODE = {
     NO_RESULT: 0,
@@ -24,21 +29,33 @@ const TASK_CODE = {
     DATA_UPDATE: 2
 };
 
+/**
+ * List of steps from where an inpector can be deployed.
+ *
+ * @enum
+ */
 export enum INSPECTOR_TYPE {
     BOOT= 'BOOT',
     POST_APP_SCAN= 'POST_APP_SCAN',
     POST_PLATFORM_SCAN= 'POST_PLATFORM_SCAN',
     POST_DEV_SCAN= 'POST_DEV_SCAN',
     ON_DEMAND= 'ON_DEMAND',
-};
+}
 
 interface ListenerList {
     [eventType :string] :StaticTask[]
 }
 
+/**
+ * A StaticTask is the component processing incoming data or executing task when an event is caught from the Bus
+ *
+ * If an analysis is not invoked explicitely by the user, but automatically when a specific event happened, then
+ * context or incoming data are processed by listeners (StaticTask).
+ *
+ * @class
+ */
 class StaticTask
 {
-
     task:any = null;
     condition:any = null;
     onDataUpdate:any = null;
@@ -47,18 +64,46 @@ class StaticTask
 
     constructor(pConfig:any=null){
         if(pConfig!==null)
-            for(let i in pConfig)
+            for(const i in pConfig)
                 this[i] = pConfig[i];
     }
 
+    /**
+     * To set a function evaluated as a condition
+     *
+     * The function MUST return a boolean
+     *
+     * @param fn {Function} Condition function
+     * @return
+     */
     setCondition(fn:any){
-        this.task = fn;
-    } 
-
-    setTask(fn:any){
         this.condition = fn;
     }
 
+
+    /**
+     * To set a function processing the context or data
+     *
+     * The function takes two arguments:
+     * - Context : the DexcaliburProject instance
+     * - Event : the event object which triggered the function, this object contains incoming data
+     *
+     *
+     * @param fn {Function} Condition function
+     * @return
+     */
+    setTask(fn:any){
+        this.task = fn;
+    }
+
+    /**
+     * Execute the task if there is not condition or if the condition is true
+     *
+     * @param ctx
+     * @param event
+     * @return {void}
+     * @method
+     */
     exec(ctx:any, event:any){
         if(this.condition != null && this.condition(ctx))
             this.task(ctx, event);
@@ -68,40 +113,188 @@ class StaticTask
 }
 
 
-
+/**
+ * An inspector is a plugin able to add (and/or):
+ * - One or more data analyzer and classifier to main Bus
+ * - One hook set containing One or more heuristics (HookStrategy) able to generate instrumentation
+ * - Additional REST endpoint to extend Dexcalibur server APIs
+ * - Addition GUI components
+ *
+ * Every analysis and builtin hooks available inside Dexcalibur are provided through Inspectors.
+ * Using Inspectors is the main way to extend Dexcalibur analyzer, features and UI
+ *
+ * All enabled inspectors are created when a project is  created/opened. However, analysis and
+ * hook strategies are executed only some specific milestone are reach during project open/create workflow.
+ * So every Inspector MUST be attached to a specific `step`. By default, the inspector is deployed when  f INSPECTOR_TYPE
+ * a new project is instantiated (open/create), so `this.step` equals `INSPECTOR_TYPE.BOOT`
+ *
+ *
+ * Inspector are responsible to :
+ * - Generate instrumentation
+ * - Process raw data returned by hook logs, in order to tag it and push it inside main Bus
+ * - Listen for data on the main Bus, an process it in order to extract new data, make correlation, or improve hooks
+ * - Filter project DBs and improve analysis
+ * - Extend graphical interface
+ * - Extend tagging system or knowledge DB
+ * - Extend REST API
+ * - Extend API
+ *
+ * @implements {BusBroadcaster}
+ * @class
+ */
 export default class Inspector implements BusBroadcaster
 {
+    /**
+     * The inspector ID is a string and it must be unique for entire engine
+     *
+     * @field
+     * @type {string}
+     * @public
+     */
     id:string = null;
+
+    /**
+     * Human-readable short name for the inspector
+     *
+     * @field
+     * @type {string}
+     * @public
+     */
     name:string = null;
+
+    /**
+     * Optional but recommended, a description of the purpose of this plugin
+     *
+     * @field
+     * @type {string}
+     * @public
+     */
     description:string = null;
 
+    /**
+     * Current context (project) for this plugin instance
+     *
+     * @field
+     * @type {DexcaliburProject}
+     * @public
+     */
     context:DexcaliburProject = null;
+
+    /**
+     * Optional. If the plugin offers heuristics (HookStrategy) to generate some hooks, then all strategies must
+     * be grouped inside a HookSet.
+     *
+     * The HookSet is deployed (executed) only when some conditions (different from parent Inspector) are fulfilled  :
+     * - at first run when a project is created, but not later because hooks will already exists
+     * - on demand, by overwriting previously generated hooks
+     * - on demand, by droping user changes
+     * - and so ...
+     *
+     *
+     * @field
+     * @type {HookSet}
+     * @public
+     */
     hookSet:HookSet = null;
+
+    /**
+     * @field
+     * @type {StaticTask[]}
+     * @public
+     * @deprecated
+     */
     staticTasks:StaticTask[] = null;
-    running:boolean = false;
+
+    /**
+     * A flag to know if the inspector has been deployed (everything executed or ready) or not.
+     *
+     * @field
+     * @type {boolean} TRUE if inspector is running, else FALSE
+     * @public
+     */
+    running = false;
+
+    /**
+     * The map of event name <> list of associated tasks.
+     *
+     * @field
+     * @type {ListenerList} Mapping of event names / tasks
+     * @public
+     */
     listener:ListenerList = {};
 
+    /**
+     * The list of events already emitted by the inspectors
+     *
+     * @field
+     * @type {string[]} A list of event name
+     * @public
+     */
     events:string[] = [];
 
-    gui_available:boolean = false;
+    /**
+     * A flag to know if the inspector has some HTML/UI material
+     *
+     * @field
+     * @type {boolean}
+     * @public
+     */
+    gui_available = false;
+
     frontController:InspectorFrontController = null;
-    preRegisteredTags:any = [];
+
+    /**
+     * A list of Tag
+     *
+     * @field
+     * @type {boolean}
+     * @public
+     */
+    preRegisteredTags = [];
+
+
+    /**
+     * A local DB - private to this plugin
+     *
+     * @field
+     * @type {IDatabase}
+     * @public
+     */
     db:IDatabase = null;
    // webapi:DelegateWebApi = null;
 
     /**
+     * A default color for hook logs and tag from this plugin
+     *
      * @type {String}
      * @field
+     * @public
      */
-    color:any = null;
+    color = null;
 
-    installed:boolean = false;
+    installed = false;
+
+
+    /**
+     * Required. The step when the plugin is deployed.
+     *
+     * Default is `INSPECTOR_TYPE.BOOT`
+     *
+     * @field
+     * @type {INSPECTOR_TYPE}
+     * @public
+     */
     step:INSPECTOR_TYPE = INSPECTOR_TYPE.BOOT;
 
-    constructor(config:any=null){
+    /**
+     *
+     * @param config
+     * @constructor
+     */
+    constructor(config=null){
 
         if(config==null){
-            for(let i in config){
+            for(const i in config){
                 this[i] = config[i];
                 if(i==="hookSet"){
                     this.id = config[i].id;
@@ -114,6 +307,12 @@ export default class Inspector implements BusBroadcaster
         return this;
     }
 
+    /**
+     * To get the UID - a unique string for the current engine instance.
+     *
+     * @return {string} The plugin UID
+     * @method
+     */
     getUID(): string {
         return this.id;
     }
@@ -130,17 +329,31 @@ export default class Inspector implements BusBroadcaster
         this.gui_available = true;
     }
 
+    /**
+     * To initialize and get the local DB as a Memory-based DB
+     *
+     * @param {DexcaliburProject} pContext
+     * @return {IDatabase} The plugin DB instance
+     * @method
+     */
     useMemoryDB(pContext:DexcaliburProject=null):IDatabase{
         if(this.context == null && pContext == null)
             throw new Error('[INSPECTOR] DB cannot be initialized because the context is not defined');
 
-        let conn:IDatabaseAdapter = ConnectorFactory.getInstance()
+        const conn:IDatabaseAdapter = ConnectorFactory.getInstance()
             .newConnector('inmemory', this.context!=null?this.context:pContext);
         this.db = conn.newTemporaryDb('insp:db:'+this.name);
 
         return this.db;
     }
 
+    /**
+     * To get the local DB of the plugin if it exists.
+     * Local DB is optional.
+     *
+     * @return {IDatabase} The plugin DB instance or null
+     * @method
+     */
     getDB():IDatabase{
         return this.db;
     }
@@ -148,6 +361,10 @@ export default class Inspector implements BusBroadcaster
     /**
      * To forward an HTTP GET request from the web server handler to the inspector front controller if available
      * Internal use
+     *
+     * @param {Express.Request} req
+     * @param {Express.Response} res
+     * @method
      */
     performGet(req:Express.Request, res:Express.Response):any{
         if(this.frontController.hasHandler(IFC_TYPE.GET)){
@@ -171,15 +388,23 @@ export default class Inspector implements BusBroadcaster
     }
 
     /**
-     * To invoke the StaticTask instances associated to the given event. 
+     * To broadcast an event from the main Bus to relevent StaticTask.
+     *
+     * This function is called every time a new data is pushed on the main Bus
+     *
+     * TODO: add asynchronous task
+     * TODO: improve performance => multi-thread ?
+     *
+     * @param {Event} event  The event from main Bus
+     * @method
      */
     broadcastEvent(event:Event){
-        let event_type:string = event.type;
+        const event_type:string = event.type;
 
         //Logger.info( event_type, this.listener[event_type]);
         //console.log(this.listener);
         if(this.listener[event_type] != null){
-            for(let i:number=0; i<this.listener[event_type].length; i++){
+            for(let i=0; i<this.listener[event_type].length; i++){
                 // TODO : async / co
                 // console.log(this.listener[event_type][i]);
                 this.listener[event_type][i].exec(this.context, event);
@@ -190,6 +415,14 @@ export default class Inspector implements BusBroadcaster
 
     /**
      * To declare new event handler. If the param `task`is  a function, a new StaticTask is create implicitly.
+     *
+     * If a function is passed as is, the resulting StaticTask will not have additional condition.
+     *
+     *
+     * @param {string} event_type  The name of the event
+     * @param {StaticTask|any} task A  StaticTask instance or the listener function.
+     * @return {Inspector} This inspector instance
+     * @method
      */
     on(event_type:string, task:StaticTask|any):Inspector{
         if(this.listener[event_type] == null)
@@ -204,12 +437,28 @@ export default class Inspector implements BusBroadcaster
     }
 
 
+    /**
+     * To add a new tag category to this plugin (to extend tagging system)
+     *
+     * @param {string} name Name of the tag category
+     * @param {any} tag The list of new tags for this category
+     * @method
+     */
     registerTagCategory(name:string, tags:any){
         this.preRegisteredTags.push({ name:name, tags:tags });
     }
 
 
     /**
+     * To initialize the plugin for the specified project
+     *
+     * At this step, every things not depending of the "starting point" are executed :
+     * - add custom tag categories to the tag system
+     * - initializing front controller / custom web api endpoints
+     * - initializing hook set
+     *
+     * @param {DexcaliburProject} ctx The current project (context)
+     * @return {Inspector} The current inspector instance
      * @method
      */
     injectContext(ctx:DexcaliburProject):Inspector{
@@ -223,7 +472,7 @@ export default class Inspector implements BusBroadcaster
         }
 
         // register front controller
-        let path = _path_.join(__dirname,"..","inspectors",this.id,"service","main.js");
+        const path = _path_.join(__dirname,"..","inspectors",this.id,"service","main.js");
         if(_fs_.existsSync(path)){
             this.frontController = require(path).default.injectContext(ctx);
             Logger.info("[Inspector::injectContext][FrontController] "+this.id+" registered !");
@@ -239,7 +488,7 @@ export default class Inspector implements BusBroadcaster
 
 
         // declare TagCategory
-        let anal = ctx.getAnalyzer();
+        const anal = ctx.getAnalyzer();
         for(let i=0; i<this.preRegisteredTags.length; i++){
             anal.addTagCategory(this.preRegisteredTags[i].name, this.preRegisteredTags[i].tags)
         }
@@ -251,6 +500,7 @@ export default class Inspector implements BusBroadcaster
         return this;
     }
 
+
     setStartStep(pStep:INSPECTOR_TYPE){
         this.step = pStep;
     }
@@ -259,13 +509,21 @@ export default class Inspector implements BusBroadcaster
         return this.step;
     }
 
-    setHookSet(hs:HookSet){
-        this.hookSet = hs;
+    /**
+     * To dynamically set the hook set.
+     *
+     * This method is used to copy some properties shared by the parent (inspector) with the child hookset.
+     *
+     * @param {HookSet} pHookSet The HookSet instance (group of hook strategy)
+     * @method
+     */
+    setHookSet(pHookSet:HookSet){
+        this.hookSet = pHookSet;
         this.hookSet.color = this.color;
 
-        if(this.id==null) this.id = hs.id;
-        if(this.name==null) this.name = hs.name;
-        if(this.description==null) this.description = hs.description;
+        if(this.id==null) this.id = pHookSet.id;
+        if(this.name==null) this.name = pHookSet.name;
+        if(this.description==null) this.description = pHookSet.description;
     }
 
     getHookSet():HookSet{
@@ -276,8 +534,18 @@ export default class Inspector implements BusBroadcaster
         return this.id;
     }
 
+    /**
+     * Deploy the current inspector.
+     *
+     * Currently, it is the function which triggers all heuristics able to generate instrumentation.
+     * It updates `running` flag to `true`.
+     *
+     * @method
+     */
     deploy():void{
         this.running = true;
+
+        // add a check to force to redeploy or not
         this.hookSet.deploy();
     }
 
@@ -296,7 +564,11 @@ export default class Inspector implements BusBroadcaster
     }*/
 
     /**
-     * emit a new event on the main event bus
+     * To emit a new event on the main event bus
+     *
+     * @param {string} name The event name
+     * @param {any}  event The event data
+     * @method
      */
     emits(name:string, event:any){
         if(this.events.indexOf(name)===-1)
@@ -318,37 +590,40 @@ export default class Inspector implements BusBroadcaster
     }
 
     /**
-     * 
+     * To restore the inspector configuration in a separate JSON file
+     *
      * @param {*} callback 
      * @method
      */
     restore(callback=null):void{
-        let self:Inspector = this;
-        let savePath:string = _path_.join(this.context.workspace.getSaveDir(), this.id+".json");
+        //let self:Inspector = this;
+        const savePath:string = _path_.join(this.context.workspace.getSaveDir(), this.id+".json");
         _fs_.lstat(savePath, (vErr:any, vStat:any)=>{
             if(vErr == null) {
                 _fs_.readFile(savePath, 'ascii', function(err, data){
 
                     if(/^[\s\t\n\r]*$/.test(data)) return ;
                     Logger.raw(data);
-                    let o = JSON.parse(data);
+                    const o = JSON.parse(data);
 
-                    self.db.unserialize(o);
+                    this.db.unserialize(o);
                     if(typeof callback === 'function')
-                        callback(self);
+                        callback(this);
                 })
             }
         });
     }
 
     /**
+     * To save the inspector configuration in a separate JSON file
+     *
      * @method
      */
     save(){
         if(this.db == null) return null;
 
-        let self:Inspector = this;
-        let savePath:string = _path_.join(this.context.workspace.getSaveDir(), this.id+".json");
+        const self:Inspector = this;
+        const savePath:string = _path_.join(this.context.workspace.getSaveDir(), this.id+".json");
         _fs_.exists(savePath, function(exist){
             if(exist){
                 _fs_.chmodSync(savePath, 0o777);
@@ -361,7 +636,7 @@ export default class Inspector implements BusBroadcaster
                     return;
                 }
 
-                let data = self.db.serialize();
+                const data = self.db.serialize();
                 _fs_.write(fd, JSON.stringify(data), function(err){
                     if(err){
                         console.log("Save file cannot be created");
@@ -380,11 +655,14 @@ export default class Inspector implements BusBroadcaster
 
     /**
      * To cast the current object to an object ready to be serialize (it avoids cyclic reference)
+     *
+     * TODO: improve listener serializing
+     *
      * @returns {Object} 
      * @method
      */
     toJsonObject():any{
-        let o:any = {};
+        const o:any = {};
         o.id = this.id;
         o.description = this.description;
         o.name = this.name;
@@ -393,7 +671,7 @@ export default class Inspector implements BusBroadcaster
         o.hooks = this.hookSet.toJsonObject();
         o.listener = [];
         o.gui_available = this.gui_available;
-        for(let i in this.listener)
+        for(const i in this.listener)
             o.listener.push({ n:i });
         
         return o;
--- src/InspectorFactory.ts
+++ src/InspectorFactory.ts
@@ -12,11 +12,23 @@ import {HookDbApi} from "./hook/HookDbApi";
 import {InspectorFactoryException} from "./errors/InspectorFactoryException";
 
 
-let Logger:Log.Logger = Log.newLogger() as Log.Logger;
+const Logger:Log.Logger = Log.newLogger() as Log.Logger;
 
+/**
+ * There is one InspectorFactory for each type prototype of Inspector.
+ *
+ * This instance is shared by every project and it is a member of DexcaliburEngine objects.
+ * Its purpose if to create Inspector instance per project
+ *
+ * @class
+ */
 export default class InspectorFactory
 {
     _config:any = null;
+
+    /**
+     * The step when the inspector must be deployed
+     */
     step:INSPECTOR_TYPE = null;
     webapi: DelegateWebApi = null;
 
@@ -41,6 +53,16 @@ export default class InspectorFactory
         return this._r;
     }
 
+    /**
+     * To create additional web api endpoints
+     *
+     * If an inspector is generated and it extends web api, then the factory must create the endpoint.
+     *
+     * TODO: [SECURITY] untrusted inspector (from user) can break isolation by defining webapi available for others projects.
+     *
+     * @param {WebServer} pWebServer
+     * @method
+     */
     registerWebServer(pWebServer:WebServer){
         if(this.hasWebApi() && !this.isWebApiReady()){
             Logger.raw("[InspectorFactory] registering web server. Plugin = ", this._config.id );
@@ -55,51 +77,68 @@ export default class InspectorFactory
         }
     }
 
+    /**
+     * To check if the inspectors make by this factory start at the specified step
+     *
+     * @param {INSPECTOR_TYPE} pStep
+     * @method
+     */
     isStartAt(pStep:INSPECTOR_TYPE):boolean{
         return this.step === pStep;
     }
 
 
     /**
-     * To create an Inspector from this prototype
+     * To create an instance of Inspector using the inspector prototype
      * 
-     * @param {*} pProject 
+     * @param {DexcaliburProject} pProject - The project instance
+     * @return {Inspector} The freshly created Inspector
+     * @method
      */
     createInstance( pProject:DexcaliburProject):Inspector{
-        let ins:Inspector = new Inspector();
-        let hmgr:HookManager = pProject.getHookManager();
-        let hapi:HookDbApi = hmgr.getDbAPI();
+        const ins:Inspector = new Inspector();
+        const hmgr:HookManager = pProject.getHookManager();
+        const hapi:HookDbApi = hmgr.getDbAPI();
         let hs:HookSet = null;
-        let hooks:Hook[] = null;
+        let strategies:any;
         let hsuid:string = null;
 
-
+        // register a new endpoint inside the web api
         if(this.hasWebApi() && !this.isWebApiReady()){
             this.registerWebServer(DexcaliburEngine.getInstance().getWebserver());
         }
 
-
+        // copy config from prototype to fresh Inspector instance
         if(this._config.id != null) ins.id = this._config.id;
         if(this._config.name != null) ins.name = this._config.name;
         if(this._config.description != null) ins.description = this._config.description;
 
+        // set "when" the inspector must be executed
         if(this._config.startStep != null){
             this.step = this._config.startStep;
             ins.setStartStep(this._config.startStep);
         }
 
+        // If specified, define the color associated to this inspector
         if(this._config.color != null){
             ins.color = this._config.color;
         }
 
+        // If the inspector prototype define a hookset, create it or restore it
         if(this._config.hookSet != null){
 
             hsuid = (this._config.id!=null ? this._config.id : this._config.hookSet.id);
 
             Logger.raw("IS STRATEGY EXISTS ?? : "+hsuid+"  "+hapi.isHookSetExists(hsuid));
+
+            // the first step is to verify if there is not yet a hook set for this project
+            // It happens mainly because the project has been created during a previous run
+            // so, data have been partially restored by the hook manager from the SQLIte db of the project earlier
             if(hapi.isHookSetExists(hsuid)){
+                // if the hook set is already instantiated, we get the instance
                 hs = hapi.getHookSet(hsuid);
             }else{
+                // else, wen create a new HookSet instance with data from Inspector prototype
                 hs = hmgr.createHookSet(hsuid, {
                     name: (this._config.hookSet.name!=null ? this._config.hookSet.name : this._config.name),
                     description: (this._config.hookSet.description!=null ? this._config.hookSet.description : this._config.description),
@@ -109,44 +148,44 @@ export default class InspectorFactory
                     builtin: true
                 });
 
-
+                // to configure object shared by several hooks
                 if(this._config.hookSet.hookShare != null){
                     hs.addHookShare(this._config.hookSet.hookShare);
                 }
 
+                // to declare hookset dependencies
                 if(this._config.hookSet.require != null){
                     this._config.hookSet.require.map((k,v)=>{
                         hs.require(k);
                     });
                 }
+                // to declare inspector dependencies inherited by hook set
                 if(this._config.require != null){
                     this._config.require.map((k,v)=>{
                         hs.require(k);
                     });
                 }
-
-
             }
 
+            // If the hook set is not initiliazed, throw a exception
             if(hs == null){
                 throw InspectorFactoryException.HOOKSET_CANNOT_BE_CREATED(this._config.name!=null ? this._config.name : this._config.hookSet.name);
             }
 
-
             // browse hook strategy
-            hooks = this._config.hookSet.hooks;
+            strategies = this._config.hookSet.strategies;
 
-            if(hooks != null){
-                hooks.map((vHookCfg)=>{
-                    if(vHookCfg.name == null){
+            if(strategies != null){
+                strategies.map((vStratCfg)=>{
+                    if(vStratCfg.name == null){
                         throw InspectorFactoryException.STRATEGY_NAME_IS_MANDATORY(ins.getUID());
                     }
 
-                    const stratUID = hsuid+":"+vHookCfg.name;
+                    const stratUID = hsuid+":"+vStratCfg.name;
                     let strat:HookStrategy = null;
 
                     if(!hapi.isStrategyExists(stratUID)){
-                        strat = HookStrategy.from(vHookCfg);
+                        strat = HookStrategy.from(vStratCfg);
 
                         strat.setUID(stratUID);
 
@@ -162,7 +201,6 @@ export default class InspectorFactory
                             );
                         }
 
-
                         hapi.createHookStrategy(strat);
 
                     }else{
@@ -177,6 +215,7 @@ export default class InspectorFactory
             hapi.updateHookSet(hs);
         }
 
+        // If the inspector use local InMemory DB : create/open it, and create indexes
         if(this._config.db != null){
             if(this._config.db.dbms==='inmemory'){
 
@@ -193,24 +232,28 @@ export default class InspectorFactory
             }   
         }
 
+        // If the inspector adds own tag categories, add them
         if(this._config.tags != null){
-            for(let i in this._config.tags){
+            for(const i in this._config.tags){
                 ins.registerTagCategory(i, this._config.tags[i]);
             }  
         }
 
+        // If the inspector use GUI, set the flag
         if(this._config.useGUI === true){
             ins.useGUI();
         }
 
+        // If the inspector extend analyzers with own event listener, register it into new Inspector
         if(this._config.eventListeners != null){
-            for(let i in this._config.eventListeners){
+            for(const i in this._config.eventListeners){
                 ins.on(i, {
                     task: this._config.eventListeners[i]
                 });
             }
         }
 
+        // Finally, when the Inspector is created from the prototype, inject the current project (context)
         ins.injectContext(pProject);
 
         return ins;
