90101142fe93da4597c8240eefa7d37a8c17dd7b
Georges-B. Michel <georges@reversense.com>
2022-10-11 15:01:28 +0200

    Add exceptions for version compatibility error

--- src/errors/DexcaliburProjectException.ts
+++ src/errors/DexcaliburProjectException.ts
@@ -5,19 +5,25 @@ export class DexcaliburProjectException extends MonitoredError {
     code:number;
     extra:any;
 
-    static ALL = {};
+    static ALL = {
+        NEED_PROJECT_UPGRADE: ErrorCode.PROJECT + 112,
+        NEED_ENGINE_UPGRADE: ErrorCode.PROJECT + 113
+    };
 
     static TARGET_DEVICE_NOT_FOUND = ()=>{ return new DexcaliburProjectException("Target device is not found", ErrorCode.PROJECT + 101) };
     static TARGET_DEVICE_NOT_ENROLLED = ()=>{ return new DexcaliburProjectException("Target device is not enrolled or ready", ErrorCode.PROJECT + 102) };
     static INVALID_NAME = ()=>{ return new DexcaliburProjectException("Project name is invalid", ErrorCode.PROJECT + 103) };
     static APP_FILE_OT_FOUND = ()=>{ return new DexcaliburProjectException("App file is not found", ErrorCode.PROJECT + 104) };
-    static STEP2_FAILURE = (pMsg:string='')=>{ return new DexcaliburProjectException("[STEP 2] Creating new project failed : "+pMsg, ErrorCode.PROJECT + 105) };
+    static STEP2_FAILURE = (pMsg='')=>{ return new DexcaliburProjectException("[STEP 2] Creating new project failed : "+pMsg, ErrorCode.PROJECT + 105) };
     static NEW_PROJECT_FAIL = ()=>{ return new DexcaliburProjectException("Project cannot be created. See logs for more details. ", ErrorCode.PROJECT + 106) };
     static OPEN_PROJECT_FAILURE = (pUID)=>{ return new DexcaliburProjectException("The project ["+pUID+"] cannot be opened", ErrorCode.PROJECT + 107) };
     static DELETE_PROJ_FAILURE_NOTFOUND = ()=>{ return new DexcaliburProjectException("Delete failure : project is not found.", ErrorCode.PROJECT + 108) };
     static INVALID_OR_NOT_ACTIVE = ()=>{ return new DexcaliburProjectException("Specified project is invalid or not active", ErrorCode.PROJECT + 109) };
     static CLOSE_PROJECT_FAILURE = ()=>{ return new DexcaliburProjectException("The project cannot be closed", ErrorCode.PROJECT + 110) };
     static NO_PROJECT_SPECIFIED = ()=>{ return new DexcaliburProjectException("There is not valid project specified.", ErrorCode.PROJECT + 111) };
+    static NEED_PROJECT_UPGRADE = (pProjVer:string,pEngVer:string)=>{ return new DexcaliburProjectException("The project workspace has been built with an oldest version ("+pProjVer+") of Dexcalibur ("+pEngVer+") , the project will be updated.", ErrorCode.PROJECT + 112) };
+    static NEED_ENGINE_UPGRADE = (pProjVer:string,pEngVer:string)=>{ return new DexcaliburProjectException("The project workspace has been built with a newest version ("+pProjVer+") of Dexcalibur ("+pEngVer+") , please upgrade the engine.", ErrorCode.PROJECT + 113) };
+
 
 
     constructor( pMsg:string, pCode:number = null, pExtra:any = null) {
