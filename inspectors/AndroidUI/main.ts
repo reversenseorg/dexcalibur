acded3245df50dd9582d0fee1b18b8eed3148083
Georges-B. Michel <georges@reversense.com>
2023-05-09 05:06:17 +0200

    [INSPECTOR] add android UI

---
+++ inspectors/AndroidUI/main.ts
@@ -0,0 +1,45 @@
+import * as HOOK from '../../src/hook/HookManager.js';
+
+import {IntentFilter} from "../../src/android/IntentFilter.js";
+import InspectorFactory from "../../src/InspectorFactory.js";
+import Inspector, {INSPECTOR_TYPE} from "../../src/Inspector.js";
+import DexcaliburProject from "../../src/DexcaliburProject.js";
+import ModelClass from "../../src/ModelClass.js";
+import BusEvent from "../../src/BusEvent.js";
+import AndroidActivity from "../../src/android/AndroidActivity.js";
+import * as Log from "../../src/Logger.js";
+import {AndroidManifest} from "../../src/android/AndroidManifest.js";
+import {AndroidCodeAnalyzer} from "../../src/android/analyzer/AndroidCodeAnalyzer.js";
+
+
+const Logger:Log.Logger = Log.newLogger() as Log.Logger;
+
+// ===== INIT =====
+
+
+
+// === CONFIG
+export default new InspectorFactory({
+
+    startStep: INSPECTOR_TYPE.POST_APP_SCAN,
+
+    useGUI: true,
+
+    tags : {
+        "input": ["text", "date", "email"],
+    },
+
+    hookSet: {
+        id: "AndroidUI",
+        name: "Android UI analyzer",
+        description: "Find, tag and hook UI components"
+    },
+
+    eventListeners: {
+        "class.new":  function (ctx:DexcaliburProject, event:BusEvent):any {
+
+        }
+    }
+});
+
+
