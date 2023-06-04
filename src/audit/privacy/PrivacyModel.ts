4441c9b4cf67814782c31e4c0bd07d03c507a499
Georges-B. Michel <georges@reversense.com>
2023-06-04 15:34:02 +0200

    Add UID and scannar ID to Assurance models

--- src/audit/privacy/PrivacyModel.ts
+++ src/audit/privacy/PrivacyModel.ts
@@ -6,6 +6,11 @@ import {TrackerInfo} from "./TrackerInfo.js";
 import {ThreatFactory} from "../common/ThreatFactory.js";
 import CodeConstraint from "../common/CodeConstraint.js";
 import {NodeInternalType} from "../../NodeInternalType.js";
+import {PII_Data} from "./assets/pii_schema.js";
+import {PiiCategory} from "./pii/PiiCategory.js";
+import {PiiClass} from "./pii/PiiClass.js";
+import {PiiField} from "./pii/PiiField.js";
+import {PiiType} from "./pii/PiiType.js";
 
 /**
  * 
@@ -13,9 +18,11 @@ import {NodeInternalType} from "../../NodeInternalType.js";
 export class PrivacyModel extends AssuranceModel {
 
 
-
     constructor( pConfig:any = null) {
-        super(pConfig);
+        super({
+            id: "privacy.generic",
+            scannerID: "scanner.privacy"
+        });
 
         if(pConfig!=null) for(const i in pConfig) this[i]=pConfig[i];
     }
@@ -79,8 +86,33 @@ export class PrivacyModel extends AssuranceModel {
         });
     }
 
+    loadPII(){
+        let  cls:PiiClass;
+        for(let name in PII_Data){
+            cls = PII_Data[name];
+            cls.categories.map( vCat => {
+                vCat.types.map( vType => {
+                    if(vType.fields.length>0){
+                        vType.fields.map( vField => {
+                            const field = new PiiField(vField);
+                            field.name = cls.name+':'+vCat.name+':'+vType.name+':'+vField.name;
+                            this.primaryAssets.push(field);
+                        });
+                    }else{
+                        const type = new PiiType(vType);
+                        type.name = cls.name+':'+vCat.name+':'+vType.name;
+                        this.primaryAssets.push( type);
+                    }
+                })
+            });
+        }
+    }
+
     load():void {
+        // load trackers as threats
         this.loadTrackersSignatures();
+        //
+        this.loadPII();
     }
 
 }
