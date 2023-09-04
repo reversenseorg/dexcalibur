c218a485f969a12e2136d420e8fa02c4f2e14bf4
Georges-B. Michel <georges@reversense.com>
2023-09-05 01:39:37 +0200

    fix dxc-agent build script to use artifacts

--- scripts/os_scripts/linux.build.sh
+++ scripts/os_scripts/linux.build.sh
@@ -15,18 +15,25 @@ if [ $DXC_USE_ARTIFACTS = 0 ]; then
   cp ../dexcalibur-agent/dxc-agent.*.min.js ./dist/agent/.
 else
 
-  mkdir ./dist/tmp
-  if [ "$DXC_CICD_API_AUTH" = "basic" ]; then
-    curl -u "$DXC_CICD_API_USER:$DXC_CICD_API_PWD" -o ./dist/tmp/artifacts.zip "$DXC_CICD_HOST/httpAuth/repository/downloadAll/$BUILD_ID_DXCAGENT/.lastSuccessful/"
+  if [[ -f ./dxc-agent.android.arm64.min.js ]] ; then
+    echo "[*] dexcalibur-agent artifacts has been set"
+    mkdir ./dist/agent
+    cp ./dxc-agent.* ./dist/agent/.
   else
-    curl --header "Authorization: Bearer $DXC_CICD_API_TOKEN" -o ./dist/tmp/artifacts.zip "$DXC_CICD_HOST/repository/downloadAll/$BUILD_ID_DXCAGENT/.lastSuccessful/"
-  fi
+    echo "[!] dexcalibur-agent files have not been copied"
+    mkdir ./dist/tmp
+      if [[ "$DXC_CICD_API_AUTH" = "basic" ]]; then
+        curl -u "$DXC_CICD_API_USER:$DXC_CICD_API_PWD" -o ./dist/tmp/artifacts.zip "$DXC_CICD_HOST/httpAuth/repository/downloadAll/$BUILD_ID_DXCAGENT/.lastSuccessful/"
+      else
+        curl --header "Authorization: Bearer $DXC_CICD_API_TOKEN" -o ./dist/tmp/artifacts.zip "$DXC_CICD_HOST/repository/downloadAll/$BUILD_ID_DXCAGENT/.lastSuccessful/"
+      fi
 
-   if [ -f ./dist/tmp/artifacts.zip ] ; then
-    echo "Artifacts download successfully, unzipping ..."
-    unzip -d ./dist/agent ./dist/tmp/artifacts.zip
-  else
-    echo "[!] Artifacts cannot be downloaded : error"
+      if [[ -f ./dist/tmp/artifacts.zip ]] ; then
+        echo "Artifacts download successfully, unzipping ..."
+        unzip -d ./dist/agent ./dist/tmp/artifacts.zip
+      else
+        echo "[!] Artifacts cannot be downloaded : error"
+      fi
   fi
 fi
 
