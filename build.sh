acfebbe6c2630b3a253e0a2ad6725d2d65e34d09
Georges-B. Michel <georges@reversense.com>
2023-09-05 03:42:51 +0200

    fix dxc-agent build script to use artifacts

--- build.sh
+++ build.sh
@@ -3,5 +3,11 @@ echo "[*] Setup env"
 # nvm use
 echo "[*] Reinstall frida-node & better-sqlite3"
 npm uninstall frida && npm install frida && npm rebuild better-sqlite3@8.0.1
+
 echo "[*] Rebuild"
-DXC_USE_ARTIFACTS=0 npm run build
+
+if [ $DXC_USE_ARTIFACTS != 1 ]; then
+  DXC_USE_ARTIFACTS=0
+fi
+
+npm run build
