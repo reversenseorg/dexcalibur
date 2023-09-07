5249043ad857e26cd74a8621cf219f75c0bc6c5f
Georges-B. Michel <georges@reversense.com>
2023-09-07 23:44:29 +0200

    Add $DXC_CICD_FRIDA_VERSION to build script

--- build.sh
+++ build.sh
@@ -3,11 +3,14 @@ echo "[*] Setup env"
 # nvm use
 echo "[*] Reinstall frida-node & better-sqlite3"
 if [[ ! -z "$DXC_CICD_FRIDA_VERSION" ]]; then
+  echo "Replace built-in frida-node by new one using DXC_CICD_FRIDA_VERSION constant : frida@$DXC_CICD_FRIDA_VERSION"
   npm uninstall frida && npm install "frida@$DXC_CICD_FRIDA_VERSION"
 else
+  echo "Replace built-in frida-node by new one with default version"
   npm uninstall frida && npm install frida
 fi
 
+echo "Rebuild better-sqlite"
 npm rebuild better-sqlite3@8.0.1
 
 echo "[*] Rebuild"
