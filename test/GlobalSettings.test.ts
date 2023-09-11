cc77944fc00f070f03b30bffb14a24f908bf618a
Georges-B. Michel <georges@reversense.com>
2023-09-11 10:07:50 +0200

    update unit test

--- test/GlobalSettings.test.ts
+++ test/GlobalSettings.test.ts
@@ -3,6 +3,7 @@ import {expect} from 'chai';
 
 import {Settings} from "../src/Settings.js";
 import GlobalSettings = Settings.GlobalSettings;
+import ServerSettings = Settings.ServerSettings;
 
 describe('GlobalSettings', function() {
 
@@ -35,11 +36,34 @@ describe('GlobalSettings', function() {
 
             const settings = new GlobalSettings();
 
-            expect(settings.getServerSettings().getHeapSize()).to.be.null;
-            expect(settings.getWebserverSettings().getWsPort()).to.be.null;
-            expect(settings.getWebserverSettings().getHttpPort()).to.be.null;
-            expect(settings.getExternalSettings().getTool("java")).to.be.null;
+            expect(settings.getServerSettings().getHeapSize()).to.equals(ServerSettings.DEFAULT_HEAP_SIZE);
+            expect(settings.getWebserverSettings().getWsPort()).to.equals(Settings.DEFAULT_WS_PORT);
+            expect(settings.getWebserverSettings().getHttpPort()).to.equals(Settings.DEFAULT_HTTP_PORT);
+            expect(settings.getExternalSettings().getTool("java")).to.be.undefined;
         });
     });
 
+    describe('toObject', function() {
+
+        it('valid settings', async function() {
+
+            const settings = new GlobalSettings({
+                bin: {
+                    java: "/bin/java"
+                },
+                server: {
+                    heapSize: 8192
+                },
+                web: {
+                    http: 8080,
+                    ws: 9999
+                }
+            });
+
+            expect(settings.getServerSettings().getHeapSize()).to.equals(8192);
+            expect(settings.getWebserverSettings().getWsPort()).to.equals(9999);
+            expect(settings.getWebserverSettings().getHttpPort()).to.equals(8080);
+            expect(settings.getExternalSettings().getTool("java")).to.equals("/bin/java");
+        });
+    });
 });
---
+++ test/ServerSettings.test.ts
@@ -0,0 +1,98 @@
+import {expect} from 'chai';
+// -- App specific --
+
+import {Settings} from "../src/Settings.js";
+import GlobalSettings = Settings.GlobalSettings;
+import ServerSettings = Settings.ServerSettings;
+import {SanitizedValue, IncomingValue} from "../src/security/SanitizedValue.js";
+import * as _path_ from "path";
+import Util from "../src/Utils.js";
+
+
+
+class MockParentSettings extends Settings.AbstractSettings {
+
+    private server:ServerSettings;
+
+    constructor() {
+        super(null);
+    }
+
+    sanitize(pName: string, pValue: any): IncomingValue {
+        return new SanitizedValue(pName, pValue);
+    }
+
+    toObject(): any {
+        return {
+          server: this.server.toObject()
+        };
+    }
+
+    update(pValue: IncomingValue): void {
+        this[pValue.getName()] = pValue.getValue();
+    }
+}
+
+describe('ServerSettings', function() {
+
+    let gEngine:Settings.GlobalSettings = null;
+
+
+    const WS_path = _path_.join(Util.__dirname(import.meta.url), 'ws');
+
+    describe('constructor', function() {
+
+        it('default settings', async function() {
+
+            const mock_settings = new GlobalSettings();
+            const settings = new ServerSettings(mock_settings);
+
+            expect(settings.getHeapSize()).to.equals(ServerSettings.DEFAULT_HEAP_SIZE);
+            expect(settings.getWorkspace()).to.be.undefined;
+            expect(settings.getRegistry()).to.be.undefined;
+            expect(settings.getAuthenticationSettings()).to.be.undefined;
+        });
+
+        it('partial settings', async function() {
+
+
+            const mock_settings = new GlobalSettings();
+            const settings = new ServerSettings(mock_settings, {
+                heapSize: 8192,
+                registry: "http://127.0.0.1:8000/",
+                registryAPI: "http://127.0.0.1:8001/",
+                workspace: WS_path,
+                auth: null
+            });
+
+            expect(settings.getHeapSize()).to.equals(8192);
+            expect(settings.getWorkspace().getLocation()).to.equals(WS_path);
+            expect(settings.getRegistry().api).to.equals("http://127.0.0.1:8001/");
+            expect(settings.getRegistry().url).to.equals("http://127.0.0.1:8000/");
+            expect(settings.getAuthenticationSettings()).to.be.undefined;
+        });
+    });
+
+    describe('toObject', function() {
+
+        it('basic', async function() {
+
+            const mock_settings = new GlobalSettings();
+            const settings = new ServerSettings(mock_settings, {
+                heapSize: 8192,
+                registry: "http://127.0.0.1:8000/",
+                registryAPI: "http://127.0.0.1:8001/",
+                workspace: WS_path,
+                auth: null
+            });
+
+            const obj = settings.toObject();
+
+            expect(obj.heapSize).to.equals(8192);
+            expect(obj.workspace).to.equals(WS_path);
+            expect(obj.registryAPI).to.equals("http://127.0.0.1:8001/");
+            expect(obj.registry).to.equals("http://127.0.0.1:8000/");
+            expect(obj.auth).to.be.undefined;
+        });
+    });
+});
