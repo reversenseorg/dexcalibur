c17f15cf091532adeec4f3a3e8f03b3e77c15b57
ph.mignot <paul-henri.mignot@reversense.com>
2024-09-25 15:16:12 +0200

    Update NativeLibraryInspector. Now able to Load library with Runtime instead of System.

--- inspectors/NativeLibrary/main.ts
+++ inspectors/NativeLibrary/main.ts
@@ -1,17 +1,13 @@
-
-
-
-// ===== INIT =====
-
 import {INSPECTOR_TYPE} from "../../src/Inspector.js";
 import InspectorFactory from "../../src/InspectorFactory.js";
 import ModelMethod from "../../src/ModelMethod.js";
+// ===== INIT =====
 
 var NativeLibraryInspector:InspectorFactory = new InspectorFactory({
 
     startStep: INSPECTOR_TYPE.POST_APP_SCAN,
 
-    version: "1.0.0",
+    version: "1.0.3",
     hookSet: {
         id: "NativeLibrary",
         name: "native library inspector",
@@ -23,18 +19,14 @@ var NativeLibraryInspector:InspectorFactory = new InspectorFactory({
             refs: {}
         },
         strategies: [{
-            name: "load",
-            descr: "To detect loading of native library",
+            name: "loadLibrary",
+            descr: "To detect loading of native library with System.loadLibrary",
             search: {
                 type: ModelMethod.TYPE.getName(),
-                uid: [
-                    "java.lang.System.load(<java.lang.String>)<void>",
-                    "java.lang.System.loadLibrary(<java.lang.String>)<void>",
-                ]
+                uid: ["java.lang.System.loadLibrary(<java.lang.String>)<void>"]
             },
-
             autoEmit: true,
-            emitEvent: "hook.nativelib.inject",
+            emitEvent: "hook.nativelib.loadLibrary.inject",
             /*
             preprocessor: ` 
                 pCtx.getInspector("NativeLibrary").emits("hook.nativelib.inject",pEvent);
@@ -43,84 +35,75 @@ var NativeLibraryInspector:InspectorFactory = new InspectorFactory({
                 ctx.getInspector("NativeLibrary").emits("hook.nativelib.inject",event);
             },*/
             replace: `
-            
-            DXC.send(
-                "@@__HOOK_ID__@@",
-                "@@__FRAG_ID__@@",
-                {
-                    path: arg0,
-                    when: 'before'
-                }
-            );
-                    
-        
-            try { 
-                const loaded = DXC.java().class.java.lang.Runtime.getRuntime().loadLibrary0(
-                    DXC.java().class.dalvik.system.VMStack.getCallingClassLoader(), arg0); 
-
-                if(arg0 === 'MY_LIB') { 
-                
-                    DXC.send(
-                        "@@__HOOK_ID__@@",
-                        "@@__FRAG_ID__@@",
-                        {
-                            path: arg0,
-                            when: 'after_MY_LIB'
-                        }
-                    );
-                } 
-                return loaded; 
-            } catch(ex) { 
-                DXC.send(
-                "@@__HOOK_ID__@@",
-                "@@__FRAG_ID__@@",
-                    {
-                        err: ex,
-                        when: 'error'
+                // <ts>={
+                // Source: https://cs.android.com/android/platform/superproject/+/android-13.0.0_r39:libcore/ojluni/src/main/java/java/lang/Runtime.java;l=978
+                let ret;
+                try { 
+                    let callingClassLoader = DXC.java().class.dalvik.system.VMStack.getCallingClassLoader();
+                    let callerClass = DXC.java().class.dalvik.system.VMStack.getStackClass2();
+                    if (callingClassLoader === null) {
+                        // Bootstrap class loader
+                        let bootstrapLoader = Java.use("java.lang.ClassLoader").getSystemClassLoader().getParent();
+                        ret = DXC.java().class.java.lang.Runtime.getRuntime().loadLibrary0(bootstrapLoader, callerClass, arg0);
+                    } else {
+                        // Custom class loader
+                        ret = DXC.java().class.java.lang.Runtime.getRuntime().loadLibrary0(callingClassLoader, callerClass, arg0);
                     }
-                );
-                
-                return null;
-            } 
-            `
-            },{
-                name: "load2",
-                descr: "To detect loading of native library",
+                    DXC.send(
+                                "@@__HOOK_ID__@@",
+                                "@@__FRAG_ID__@@",
+                                {library: arg0, message: 'Successfully load library with Runtime, instead of System'}
+                    );
+                } catch(ex) {
+                    // Call original method
+                    DXC.send(
+                                "@@__HOOK_ID__@@",
+                                "@@__FRAG_ID__@@",
+                                {library: arg0, message: 'Could not load library with Runtime. Using System loadLibrary'}
+                    );
+                    ret = meth_@@__METHDEF__@@.call(this, arg0);
+                } 
+                return ret;
+                `
+            },
+            {
+                name: "load",
+                descr: "To detect loading of native library with System.load on a filename",
                 search: {
                     type: ModelMethod.TYPE.getName(),
-                    uid: [
-                        "java.lang.Runtime.load(<java.lang.String>)<void>",
-                        "java.lang.Runtime.loadLibrary(<java.lang.String>)<void>"
-                    ]
+                    uid: ["java.lang.System.load(<java.lang.String>)<void>"]
                 },
-                /*onMatch: function(ctx:DexcaliburProject,event:Event):any{
-                    ctx.getInspector("NativeLibrary").emits("hook.nativelib.load", event);
-                },
-                preprocessor: ` 
-                    pCtx.getInspector("NativeLibrary").emits("hook.nativelib.load",pEvent);
-                `,*/
                 autoEmit: true,
-                emitEvent: "hook.nativelib.load",
-                before: `
-                
-                    DXC.send(
-                    "@@__HOOK_ID__@@",
-                    "@@__FRAG_ID__@@",
-                        {
-                            path:arg0,
-                            __msg__:"@@__METHSIGN__@@"
-                        }
-                        
-                    );
+                emitEvent: "hook.nativelib.load.inject",
+                replace: `
+                    // <ts>={
+                    // Source: https://cs.android.com/android/platform/superproject/+/android-13.0.0_r39:libcore/ojluni/src/main/java/java/lang/Runtime.java;l=978
+                    // https://cs.android.com/android/platform/superproject/+/android14-qpr3-release:libcore/ojluni/src/main/java/jdk/internal/reflect/Reflection.java;l=74?q=getCallerClass&sq=&ss=android%2Fplatform%2Fsuperproject
+                    let ret;
+                    try {
+                        let callerClass = DXC.java().class.dalvik.system.VMStack.getStackClass2();
+                        ret = DXC.java().class.java.lang.Runtime.getRuntime().load0(callerClass, arg0);
+                        DXC.send(
+                            "@@__HOOK_ID__@@",
+                            "@@__FRAG_ID__@@",
+                            {library: arg0, message: 'Successfully load filename as a library with Runtime, instead of using System load'}
+                        );
+                    } catch(ex) {
+                        DXC.send(
+                            "@@__HOOK_ID__@@",
+                            "@@__FRAG_ID__@@",
+                            {filename: arg0, message: 'Could not load filename as library with Runtime. Using System load'}
+                        );
+                        ret = meth_@@__METHDEF__@@.call(this, arg0);
+                    } 
+                    return ret;
                 `
             }
-
         ]
     },
-
     eventListeners: {
         /*"hook.nativelib.load": function(ctx:any,event:any):any{
-            //todo
+            //TODO
         }*/
     }
 });
