47d776c4abb7a04217ad324adfc0f06f6dc5cdec
ph.mignot <paul-henri.mignot@reversense.com>
2024-09-26 17:38:02 +0200

    Add PhysicalEventChannel.ts

---
+++ src/deviceEvent/PhysicalEventChannel.ts
@@ -0,0 +1,18 @@
+import EventRecordSession from "./EventRecordSession.js";
+import InputSubsystem from "./InputSubsystem.js";
+
+export default class PhysicalEventChannel {
+
+    records: EventRecordSession[];
+    subsystemType: InputSubsystem;
+
+    constructor( pConfig:any = null) {
+        if(pConfig!=null) for(const i in pConfig) this[i]=pConfig[i];
+    }
+
+    static startRecord(): EventRecordSession {
+        let newRecord = new EventRecordSession();
+        this.records.push(newRecord);
+        return newRecord;
+    }
+}
