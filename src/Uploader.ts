87a02182a89fc4c5cfc26d114997e1fb76d90b51
georges.michel <georges@reversense.com>
2025-01-14 01:35:20 +0100

    Make uploader stateless

--- src/Uploader.ts
+++ src/Uploader.ts
@@ -6,6 +6,11 @@ import * as _busboy_ from 'busboy';
 import Util from './Utils.js';
 import * as Express from 'express';
 import DexcaliburWorkspace from "./DexcaliburWorkspace.js";
+import {NodeInternalType, Nullable} from "@dexcalibur/dxc-core-api";
+import DexcaliburEngine from "./DexcaliburEngine.js";
+import {UploadedResource, UploadedResourceUUID} from "./common/UploadedResource.js";
+import {UserAccount} from "./user/UserAccount.js";
+import {MongodbDbCollection} from "@dexcalibur/dexcalibur-orm-mongodb";
 
 const BUSBOY = _busboy_.default;
 
@@ -20,18 +25,24 @@ var gInstance:Uploader = null;
  */
 export default class Uploader
 {
+    private _ctx:DexcaliburEngine;
+
     uploads:IUploadSession = null;
 
-    constructor(){
+    constructor(pEngine:DexcaliburEngine){
         this.uploads = {};
+        this._ctx = pEngine;
     }
 
     /**
      * @method
      */
-    static getInstance():Uploader{
+    static getInstance(pEngine:Nullable<DexcaliburEngine> = null):Uploader{
         if(gInstance == null){
-            gInstance = new Uploader();
+            if(pEngine==null){
+                throw new Error('Cannot create Uploader : context is mandatory');
+            }
+            gInstance = new Uploader(pEngine);
         }
 
         return gInstance;
@@ -42,10 +53,44 @@ export default class Uploader
      * @param {*} pUploadId 
      * @method
      */
-    getPathOf( pUploadId:string):string{
-        return this.uploads[pUploadId];
+    async getPathOf( pUploadId:UploadedResourceUUID):Promise<string>{
+       const res = await this.getResource(pUploadId);
+
+       if(res==null){
+           throw new Error("Uploaded resource not found : "+pUploadId);
+       }
+
+       return res.path;
     }
 
+    /**
+     *
+     * @param {*} pUploadId
+     * @method
+     */
+    async getResource( pUploadId:UploadedResourceUUID):Promise<UploadedResource>{
+        return await (this._ctx.getEngineDB()
+            .getCollectionOf(UploadedResource.TYPE.getType()) as MongodbDbCollection)
+            .asyncGetEntry({ uuid: pUploadId });
+    }
+
+    /*
+    get PathOf( pUploadId:string):string{
+
+        return this.uploads[pUploadId];
+    }
+     */
+
+    async save(pRes:UploadedResource, pFields:string[] = []):Promise<any> {
+        if(pFields.length>0){
+            return await (this._ctx.getEngineDB()
+                .getCollectionOf(UploadedResource.TYPE.getType()) as MongodbDbCollection)
+                .asyncUpdateEntry( pRes, {replace:false, $set:pFields });
+        }else{
+            return await this._ctx.getEngineDB().save(pRes);
+        }
+
+    }
     /**
      * 
      * @param {*} pRequest 
@@ -53,16 +98,29 @@ export default class Uploader
      * @param {*} pCallbacks 
      * @method
      */
-    newUpload( pRequest:Express.Request, pResponse:Express.Response, pOnFinish:any){
+    async newUpload( pUserAccount:UserAccount, pRequest:Express.Request, pResponse:Express.Response, pOnFinish:any){
 
         let busboy:any = new BUSBOY({ headers: pRequest.headers });
+
+        const upl = new UploadedResource({
+            uuid: await this._ctx.getEngineDB().generateFreeUuid(NodeInternalType.UPLOAD)
+        })
+
         let id:string = Util.randString(16, Util.ALPHANUM);
 
         let saveTo:string = _path_.join(
             DexcaliburWorkspace.getInstance().getTempFolderLocation(), 
             'apk_'+id);
 
+        // deprecated because stateful
         this.uploads[id] = saveTo;
+
+        upl.path = saveTo;
+        upl.date = (new Date()).getTime();
+        upl.setOwner(pUserAccount);
+
+        let self = this;
+        await this._ctx.getEngineDB().save(upl);
 
         (pRequest as any).dxc.sess.addData('proj_upload_id', id);
 
@@ -71,7 +129,13 @@ export default class Uploader
         });
 
         busboy.on('finish', function(){
-            pOnFinish(id);
+            (async ()=>{
+                upl.terminate();
+
+                await self.save(upl, ['terminated']);
+
+                pOnFinish(upl);
+            })();
         });
 
         return pRequest.pipe(busboy);
---
+++ src/common/UploadedResource.ts
@@ -0,0 +1,119 @@
+import {
+    DbDataType,
+    DbKeyType,
+    INode,
+    NodeProperty, NodePropertyState,
+    NodeType,
+    SerializeOptions,
+    TagUUID
+} from "@dexcalibur/dexcalibur-orm";
+import {NodeInternalType} from "@dexcalibur/dxc-core-api";
+import {Auditable} from "../Auditable.js";
+import {SecurityZone} from "../security/SecurityZone.js";
+import {OrganizationAccessControl} from "../user/acl/rbac/OrganizationAccessContol.js";
+import {ValidationRule} from "../Validator.js";
+import {GlobalAccessControl} from "../user/acl/rbac/GlobalAccessContol.js";
+import {AccessAttribute, AccessAttributeMap} from "../user/acl/AccessAttribute.js";
+
+export type  UploadedResourceUUID = string;
+
+export interface UploadedResourceOpts {
+    _id?:string;
+    uuid?:UploadedResourceUUID;
+    path?:string;
+    date?:number
+}
+
+/**
+ *
+ */
+export class UploadedResource extends Auditable implements INode {
+
+    static VALIDATE:Record<string, ValidationRule> = {
+        uuid: ValidationRule.uuid()
+    }
+
+    /**
+     *
+     */
+    static TYPE:NodeType = new NodeType(
+        "upload",
+        NodeInternalType.UPLOAD,
+        [
+            (new NodeProperty("_id")).type(DbDataType.STRING).key(DbKeyType.PRIMARY),
+            (new NodeProperty("uuid")).type(DbDataType.STRING).key(DbKeyType.PRIMARY),
+            (new NodeProperty("path")).type(DbDataType.STRING),
+            (new NodeProperty("date")).type(DbDataType.NUMERIC).def(0),
+            (new NodeProperty("terminated")).type(DbDataType.BOOLEAN),
+            (new NodeProperty("tags")).type(DbDataType.BLOB).def([]),
+            (new NodeProperty("_attr"))
+                .type(DbDataType.STRING)
+                .wakeUp( (x:NodePropertyState) => {
+                    if(x.p!=null){
+                        const m:AccessAttributeMap = {};
+                        for(let k in x.p){
+                            m[k] = AccessAttribute.from({
+                                name: x.p[k]._n,
+                                value: x.p[k]._v,
+                            });
+                        }
+                        return m;
+                    }else{
+                        return {};
+                    }
+                })
+                .def({})
+        ]);
+    __ = NodeInternalType.UPLOAD;
+
+    _id:string;
+
+    uuid:UploadedResourceUUID;
+
+    path:string;
+
+    terminated = false;
+
+    date:number = -1
+
+    tags:TagUUID[] = [];
+
+    constructor(pOptions:UploadedResourceOpts = {}) {
+        super({});
+
+        if(pOptions._id!=null) this._id = pOptions._id;
+        if(pOptions.uuid!=null) this.uuid = pOptions.uuid;
+        if(pOptions.path!=null) this.path = pOptions.path;
+        if(pOptions.date!=null) this.date = pOptions.date;
+    }
+
+    initAccessAttributes() {
+        this.setAccessAttribute(GlobalAccessControl.attr.OWNER);
+    }
+
+    getUID():UploadedResourceUUID {
+        return this.uuid;
+    }
+
+    setOwner(pNode:INode):void {
+        this.appendToAccessAttribute(
+            GlobalAccessControl.attr.OWNER,
+            pNode.getUID()
+        );
+    }
+
+    toJsonObject(pOption?: SerializeOptions, pZone = SecurityZone.PUBLIC): any {
+        return {
+            _id: this._id,
+            path: (pZone===SecurityZone.PRIVATE? this.path : null),
+            uuid: this.uuid,
+            date: this.date,
+            terminated: this.terminated,
+        }
+    }
+
+    terminate() {
+        this.terminated = true;
+    }
+}
+UploadedResource.TYPE.builder(UploadedResource);
