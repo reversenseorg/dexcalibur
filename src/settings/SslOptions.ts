1103d3b2c4cea13e431ed3909a13a9e65fa312eb
georges.michel <georges@reversense.com>
2025-01-14 01:36:56 +0100

    Add SSL settings and allow SSL between master and slaves nodes

---
+++ src/settings/SslOptions.ts
@@ -0,0 +1,204 @@
+import * as _fs_ from "fs"
+import {IncomingValue, SanitizedValue, UnsafeValue} from "../security/SanitizedValue.js";
+import {ValidationRule} from "../Validator.js";
+import {AbstractSettings} from "./AbstractSettings.js";
+
+export enum SslCertFmt {
+    BLOB=0,
+    PATH
+}
+
+/**
+ * Represents SSL configuration options for establishing secure connections.
+ * @interface
+ */
+export interface SslOptions {
+    /**
+     * Flag to enforce SSL or not.
+     * If TRUE, clear-text traffic over "http" is forbidden.
+     * @type {boolean}
+     */
+    forceHttps?: boolean;
+
+    /**
+     * The SSL certificate path or content.
+     *
+     * @type {string|undefined}
+     */
+    sslCert?: string;
+
+    /**
+     * The format of SSL certificate : a BLOB of a PATH
+     *
+     * @type {SslCertFmt}
+     */
+    certFmt?:SslCertFmt;
+
+    /**
+     * The SSL Key for establishing a secure connection.
+     *
+     * @type {string}
+     */
+    sslKey?: string;
+
+    /**
+     * The protocols variable represents the available transport layer security (TLS) protocols.
+     *
+     * @type {string}
+     * @typedef {('TLSv1.2'|'TLSv1.3')} Protocols
+     * @description The value of the protocols variable can be either 'TLSv1.2' or 'TLSv1.3',
+     * representing the version of the TLS protocol that should be used for secure communication.
+     * TLSv1.2 and TLSv1.3 are the currently supported versions of TLS.
+     */
+    protocols?:"TLSv1.2"|"TLSv1.3";
+
+    /**
+     * Extra options see `https.ServerOptions<any,any>`from `https`module
+     * @type {https.ServerOptions}
+     */
+    extraOptions?:any;
+}
+
+/**
+ * Class to store SSL settings
+ * @class
+ */
+export class SslSettings extends AbstractSettings {
+
+
+    static VALIDATE = {
+        forceHttps: ValidationRule.bool(),
+        sslCert: ValidationRule.utf8String(),
+        certFmt: ValidationRule.newPinklistAssert([SslCertFmt.BLOB,SslCertFmt.PATH]),
+        sslKey: ValidationRule.utf8String(),
+        protocols: ValidationRule.newPinklistAssert(["TLSv1.2","TLSv1.3"]),
+        extraOptions: ValidationRule.nullableObj(),
+    }
+    /**
+     * Flag to enforce SSL or not.
+     * If TRUE, clear-text traffic over "http" is forbidden.
+     * @type {boolean}
+     */
+    forceHttps: boolean = true
+
+    /**
+     * The path of a file containing the SSL certificate path or content.
+     *
+     * @type {string|undefined}
+     */
+    sslCert: string = "";
+
+    /**
+     * The format of SSL certificate : a BLOB of a PATH
+     *
+     * @type {SslCertFmt}
+     */
+    certFmt: SslCertFmt = SslCertFmt.BLOB;
+
+    /**
+     * The path of a file containing the SSL Key for establishing a secure connection.
+     *
+     * @type {string}
+     */
+    sslKey: string = "";
+
+    /**
+     * The protocols variable represents the available transport layer security (TLS) protocols.
+     *
+     * @type {string}
+     * @typedef {('TLSv1.2'|'TLSv1.3')} Protocols
+     * @description The value of the protocols variable can be either 'TLSv1.2' or 'TLSv1.3',
+     * representing the version of the TLS protocol that should be used for secure communication.
+     * TLSv1.2 and TLSv1.3 are the currently supported versions of TLS.
+     */
+    protocols = "TLSv1.3";
+
+    /**
+     * Extra options see `https.ServerOptions<any,any>`from `https`module
+     * @type {https.ServerOptions}
+     */
+    extraOptions:any = {}
+
+    /**
+     *
+     * @param pOptions
+     */
+    constructor(pParent:AbstractSettings, pOptions:SslOptions) {
+        super(pParent);
+
+        if(pOptions.forceHttps!=null) this.forceHttps = pOptions.forceHttps;
+        if(pOptions.sslCert!=null) this.sslCert = pOptions.sslCert;
+        if(pOptions.sslKey!=null) this.sslKey = pOptions.sslKey;
+        if(pOptions.protocols!=null) this.protocols = pOptions.protocols;
+        if(pOptions.certFmt!=null) this.certFmt = pOptions.certFmt;
+        if(pOptions.extraOptions!=null) this.extraOptions = pOptions.extraOptions;
+    }
+
+    isEnforcing():boolean {
+        return this.forceHttps;
+    }
+
+    getProtocols():string {
+        return this.protocols;
+    }
+
+    /**
+     * To get the SSL certficate to
+     *
+     * @returns {Buffer} A buffer containing the certificate
+     * @method
+     */
+    getSslCert():Buffer {
+        switch (this.certFmt){
+            case SslCertFmt.PATH:
+                if(!_fs_.existsSync(this.sslCert)){
+                    throw new Error("File of SSL Certifcate noy found");
+                }
+                return Buffer.from(_fs_.readFileSync(this.sslCert,{encoding:'hex'}),'hex');
+            case SslCertFmt.BLOB:
+                return Buffer.from(this.sslCert,'hex');
+            default:
+                throw new Error("Ssl Certifcate format not supported");
+                break;
+        }
+    }
+
+    sanitize(pName: string, pValue: any): IncomingValue {
+
+        if(SslSettings.VALIDATE[pName]==null){
+            return new UnsafeValue(pName,pValue);
+        }
+
+        if(SslSettings.VALIDATE[pName].test(pValue)){
+            return new SanitizedValue(pName, pValue);
+        }else{
+            return new UnsafeValue(pName,pValue);
+        }
+    }
+
+
+    update(pValue: IncomingValue) {
+        if(SslSettings.VALIDATE[pValue.getName()]!=null){
+            this[pValue.getName()] = pValue.getValue();
+        }
+    }
+
+    toObject(): any {
+        return this.toJsonObject();
+    }
+
+    /**
+     * @method
+     */
+    toJsonObject():any{
+        return {
+            forceHttps: this.forceHttps,
+            sslCert: this.sslCert,
+            sslKey: this.sslKey,
+            protocols: this.protocols,
+            certFmt: this.certFmt,
+            extraOptions: this.extraOptions
+        };
+    }
+}
+
